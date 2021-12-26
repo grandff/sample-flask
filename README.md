e6ada6d876d764866029ba4fc36b1abc7b76510a
master <dsaqaz421@naver.com>
2021-12-26 15:37:29 +0900

    01 sample complete

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sample Flask Source
 
 ## reference
-https://krksap.tistory.com/1766
 https://justkode.kr/python/flask-restapi-2
 https://towardsdatascience.com/a-flask-api-for-serving-scikit-learn-models-c8bcdaa41daa
+https://velog.io/@awesome-hong/VScode%EC%97%90%EC%84%9C-%EA%B0%80%EC%83%81%ED%99%98%EA%B2%BD-%EC%84%A4%EC%A0%95-Flask-%EC%84%A4%EC%B9%98%ED%95%98%EA%B8%B0
--- app.py
+++ app.py
@@ -1,14 +1,63 @@
-from flask import Flask         # 서버 구현을 위한 객체
+from flask import Flask, request         # 서버 구현을 위한 객체
 from flask_restx import Api, Resource   # api 구현을 위한 객체
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask(__name__)   # flask 객체 선언, 파라미터로 어플리케이션 패키지 이름 넣어줌
+api = Api(app)  # flask 객체에 api 객체 등록
 
-@api.route('/hello')
+@api.route('/hello')        # /hello 경로에 클래스 등록
 class HelloWorld(Resource) :
-    def get(self) :
+    def get(self) :     # get 요청시 리턴 값에 해당하는 dict를 json 형태로 변환
         return {
             "hello" : "world!"
+        }
+
+# url pattern
+@api.route('/hello2/<string:name>')     # url pattern으로 name 설정
+class Hello(Resource) :
+    def get(self, name):
+        return {
+            "message" : "Welcome, %s" % name
+        }
+
+# get post put delete 구현
+todos = {}
+count = 1
+
+# post
+@api.route('/todos')
+class TodoPost(Resource) :
+    def post(self) :
+        global count
+        global todos
+
+        idx = count
+        count += 1
+        todos[idx] = request.json.get('data')      # json data get
+
+        return {
+            'todo_id' : idx,
+            'data' : todos[idx]
+        }
+
+@api.route('/todos/<int:todo_id>')
+class TodoSimple(Resource) :
+    def get(self, todo_id) :        # 현재 todos에 저장되어있는 값 확인
+        return {
+            'todo_id' : todo_id,
+            'data' : todos[todo_id]
+        }
+
+    def put(self, todo_id) :
+        todos[todo_id] = request.json.get('data')
+        return {
+            'todo_id' : todo_id,
+            'data' : todos[todo_id]
+        }
+
+    def delete(sel, todo_id) :
+        del todos[todo_id]
+        return {
+            "delete" : "success"
         }
 
 if __name__ == "__main__" :
